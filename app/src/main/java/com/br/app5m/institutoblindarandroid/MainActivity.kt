2ee153320ab017acbb4050c682556c30ac7520eb
joao capitaneo <joaocapitaneo@gmail.com>
2022-02-22 14:38:44 -0300

    foi

---
+++ app/src/main/java/com/br/app5m/institutoblindarandroid/MainActivity.kt
@@ -0,0 +1,104 @@
+package com.br.app5m.institutoblindarandroid
+
+import android.os.Bundle
+import android.view.View
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.br.app5m.institutoblindarandroid.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navController: NavController
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        navController = findNavController(R.id.main_nav_host) //Initialising navController
+
+        appBarConfiguration = AppBarConfiguration.Builder( R.id.myCallsFrag,
+            R.id.mainMenuFrag, R.id.navigation_home) //Pass the ids of fragments from nav_graph which you d'ont want to show back button in toolbar
+            .build()
+        setSupportActionBar(binding.mainToolbar) //Set toolbar
+
+        setupActionBarWithNavController(navController, appBarConfiguration) //Setup toolbar with back button and drawer icon according to appBarConfiguration
+
+
+        visibilityNavElements(navController) //If you want to hide drawer or bottom navigation configure that in this function
+    }
+
+    private fun visibilityNavElements(navController: NavController) {
+
+        //Listen for the change in fragment (navigation) and hide or show drawer or bottom navigation accordingly if required
+        //Modify this according to your need
+        //If you want you can implement logic to deselect(styling) the bottom navigation menu item when drawer item selected using listener
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+           /*     R.id.mainMenuFrag -> hideBothNavigation()
+                R.id.myCallsFrag -> hideBottomNavigation()*/
+                else -> showBothNavigation()
+            }
+        }
+
+    }
+
+    private fun hideBothNavigation() { //Hide both drawer and bottom navigation bar
+        binding.mainBottomNavigationView.visibility = View.GONE
+    }
+
+    private fun hideBottomNavigation() { //Hide bottom navigation
+        binding.mainBottomNavigationView.visibility = View.GONE
+
+    }
+
+    private fun showBothNavigation() {
+        binding.mainBottomNavigationView.visibility = View.VISIBLE
+
+        setupNavControl() //To configure navController with drawer and bottom navigation
+    }
+
+    private fun setupNavControl() {
+        binding.mainBottomNavigationView.setupWithNavController(navController) //Setup Bottom navigation with navController
+    }
+
+    fun exitApp() { //To exit the application call this function from fragment
+        this.finishAffinity()
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = navController
+        return when(navController.currentDestination?.id) {
+            R.id.navigation_home -> {
+                finish()
+                true
+            }
+            else -> navController.navigateUp()
+        }
+        //Setup appBarConfiguration for back arrow
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+    }
+
+/*    override fun onBackPressed() {
+
+        when { //If drawer layout is open close that on back pressed
+
+
+            }
+            else -> {
+                super.onBackPressed() //If drawer is already in closed condition then go back
+            }
+        }
+    }*/
+}
